9713ed31adfdd5292fe30a427224d6f0288a73f2
c-marv <yerson.copa@gmail.com>
2024-08-31 18:37:07 -0400

    Added pagination to search command

--- mongo-init.js
+++ mongo-init.js
@@ -1,10 +1,10 @@
 db.createUser({
-    user: _getEnv('MONGO_DB_USER'),
-    pwd: _getEnv('MONGO_DB_PASSWORD'),
-    roles: [
-        {
-            role: 'dbOwner',
-            db: _getEnv('MONGO_DB_NAME'),
-        }
-    ]
+  user: _getEnv('MONGO_DB_USER'),
+  pwd: _getEnv('MONGO_DB_PASSWORD'),
+  roles: [
+    {
+      role: 'dbOwner',
+      db: _getEnv('MONGO_DB_NAME'),
+    }
+  ]
 });
--- src/commands/on-search.command.js
+++ src/commands/on-search.command.js
@@ -1,5 +1,6 @@
 const { TelegramResource } = require("../db");
 
+const RESULT_LIMIT = 5;
 /**
  * Handles the inline search query and returns matching GIFs based on tags.
  * @param {import('telegraf').Context} ctx - The context object containing information about inline query.
@@ -9,15 +10,25 @@ module.exports = async (ctx) => {
   if (ctx.inlineQuery.query.length < 1) {
     return ctx.answerInlineQuery([]);
   }
-  const result = await TelegramResource.find({
+
+  const currentPage = ctx.inlineQuery.offset ? parseInt(ctx.inlineQuery.offset) : 0;
+
+  const query = {
     userId: ctx.inlineQuery.from.id,
     tags: {
-      $regex: '^' + ctx.inlineQuery.query
+      $elemMatch: {
+        $regex: '^' + ctx.inlineQuery.query,
+        $options: 'i',
+      }
     },
-  });
+  };
+
+  const result = await TelegramResource.find(query).skip(currentPage * RESULT_LIMIT).limit(RESULT_LIMIT);
   return await ctx.answerInlineQuery(result.map(item => ({
     type: 'gif',
     id: item._id,
     gif_file_id: item.fileId,
-  })));
+  })), {
+    next_offset: (currentPage + 1),
+  });
 };
